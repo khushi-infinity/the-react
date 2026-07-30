26c9bdea2536f4836b5f0e7ec3c9212273dab7a4
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-30 14:40:21 +0530

    added middleware and post to notes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,7 @@
 //starts a server
-const app = require('./src/app');
+const app = require('./src/app.js');
 
-app.get('/', (req, res)=>{
-    res.send('Hello World!')
-})
 
-app.get('/about', (req, res)=>{
-    res.send('About Page')
-})
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
--- backend/src/app.js
+++ backend/src/app.js
@@ -2,5 +2,21 @@
 const express = require('express');
 
 const app = express();
+//use a middleware as the express do not understand the body request 
+app.use(express.json());
+
+const notes = []
+
+/* title, description */
+app.post('/notes', (req, res)=>{
+    //pushed the request body to the notes array
+    notes.push(req.body);
+
+    // sends the response as created successfully with status code 201
+    res.status(201).json({
+        message: 'Note created successfully'
+    })
+})
+
 
 module.exports = app;
