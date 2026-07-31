bb0a4d1f446e5cb0a8017d967081ccaebbe0aad2
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-31 11:55:53 +0530

    feat: add dotenv support and implement image upload functionality

--- projectbackend/src/app.js
+++ projectbackend/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const multer = require('multer');
+const uploadFile = require('./services/storage.service')
+const postModel = require('./models/post.model')
 
 const app = express();
 app.use(express.json());
@@ -10,7 +12,21 @@ const upload = multer({storage: multer.memoryStorage()});
 app.post('/create-post',upload.single("image"), async(req,res)=>{
 console.log(req.body)
 console.log(req.file)
+
+const result = await uploadFile(req.file.buffer)
+
+const post = await postModel.create({
+    image: result.url,
+    caption: req.body.caption   
 })
+
+return res.status(201).json({
+    message: "Post created successfully",
+    post
+})
+
+})
+
 
 
 module.exports = app;
